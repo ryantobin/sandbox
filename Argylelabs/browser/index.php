a19f8fd7c671cecaf195ac1d26408ac1a14b4cac
Ryan <ryan@tobin.ca>
2014-02-25 21:26:35 -0500

    Argyle Labs homepage merge from laptop

---
+++ Argylelabs/browser/index.php
@@ -0,0 +1,28 @@
+<?php
+// index.php
+
+?>
+<!-- <iframe src="../backgrounds.php"> -->
+<iframe src="http://www.google.ca">
+
+
+</iframe>
+
+<?php
+
+?>
+
+<style>
+
+iframe {
+	width: 100%;
+	height: 800px;
+	margin: 0 auto;
+	padding-top: 50px;
+	border: 
+	-moz-box-shadow: 10px 10px 5px #888;
+	-webkit-box-shadow: 10px 10px 5px #888;
+	box-shadow: 10px 10px 5px #888;
+}
+
+</style>
