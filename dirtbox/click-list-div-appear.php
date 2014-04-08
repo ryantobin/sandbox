a4b7a5f1ea91b7e9522d5c6cc9f826515d9c281b
ryan <ryan@proximify.ca>
2014-04-08 09:54:03 -0400

    adding the js to the click-list-div-appear.php file

--- dirtbox/click-list-div-appear.php
+++ dirtbox/click-list-div-appear.php
@@ -23,4 +23,42 @@
 
 <script>
 
+// add span around li's
+var lis = document.getElementsByTagName('li');
+
+// Array.forEach(function() {}); // Requires ECMASCript5
+lis.forEach
+(
+
+	console.log(this);
+
+	function () 
+	{
+		this.innerHTML = "<span>"+this.innerText.toLowerCase()+"</span>";
+	}
+
+);
+
+/** // From proximify auth-api //
+var actionsList = $('#actions li span');
+
+var actionList = document.getElementById('api-actions').getElementsByClassName('disc-list')[0].getElementsByTagName('span');
+console.log(actionsList);
+
+var actionName;
+
+for(var i = 0; i < actionsList.length; i++) {
+	//var actionName = actionList[i].name.substring(actionList[i].name.indexOf('#')+1);
+	actionName = actionList[i].attributes['name'].value.substring(actionList[i].attributes['name'].value.indexOf('#')+1);
+	//actionsList[i].href = "x";
+	//actionList[i].click(function () 
+	actionList[i].addEventListener('click', printName(actionList[i]), false);
+}
+
+function printName(elem) {
+	console.log('you clicked: '+elem.attributes['name'].value);
+}
+
+*/
+
 </script>
