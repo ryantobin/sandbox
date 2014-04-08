e920d3f6c49ad774ccc38416e116418853f10218
Ryan <ryan@tobin.ca>
2014-04-07 23:53:07 -0400

    getting wow site going div/appear/tabbing php/js file setup

---
+++ dirtbox/click-list-div-appear.php
@@ -0,0 +1,26 @@
+
+<section id='actions'>
+	<div class='list'>
+
+		<ul>
+			<li>getTitles</li>
+			<li>getUnits<li>
+			<li>getRoles</li>
+		</ul>
+
+	</div>
+
+	<div id='gettitles'>
+		This is the getTitles div
+	</div>
+	<div id='getUnits'>
+		This is the getUnits div
+	</div>
+	<div id='getRoles'>
+		This is the getRoles div
+	</div>
+</section>
+
+<script>
+
+</script>
--- wow/druid_macros.php
+++ wow/druid_macros.php
@@ -45,6 +45,69 @@
 /use Battle Standard of Coordination
 
 
+// Genesis
+
+/castsequence Genesis
+/cast Nourish
+
+
+// Healing Touch
+
+#showtooltip
+/cast [mod:alt,@player][@mouseover, help, nodead] [help]Healing Touch; [@mouseover, harm][harm] [] Starfire;
+/castsequence [@mouseover, harm][harm] [] Starsurge;
+
+
+// HT/Wrath
+
+#showtooltip
+/use [mod:alt] Horde Battle Standard;
+/use [mod:alt] Lifeblood;
+/cast [mod:alt] Life Spirit;
+/cast [mod:alt, @player][help] Nature's Swiftness;
+/cast [mod:alt,@player][@mouseover, help][help]  Healing Touch;[harm] Wrath; [] Healing Touch
+
+
+// Leap
+
+#showtooltip
+/cancelform [form:4,@mouseover, help][noform:4,@mouseover,help]
+/cast [noform:3,@mouseover, harm] Bear Form;
+/cast [@mouseover, help] Wild Charge; /cast [@mouseover, harm] Wild Charge; [noform:4, help] Wild Growth; Wild Charge
+
+
+// Lifebloom
+
+/cast 
+[modifier:alt, target=player][target=mouseover, help][help] Lifebloom; 
+[form:3,harm] Rake; 
+[form:1, harm] Swipe; 
+[@focus][] Lifebloom
+
+
+// Mangle/Maul
+
+#showtooltip
+/cast [mod:alt, @player][@mouseover, help][help] Regrowth; 
+/cast [form:1] Maul
+/stopcasting [form:1]
+/cast [form:1/3] Mangle
+
+
+// Moonfire/Rejuv
+
+#showtooltip
+/cast [mod:alt,@player][@mouseover, help, nodead] [help]Rejuvenation; 
+/cast [@mouseover, harm][noform:3,harm][harm]  Moonfire
+/cast [form:3,harm] Shred; Rejuvenation
+
+
+// Natures Swiftness/Bite
+
+/cast [mod:alt, @player][@mouseover, help][help] Nature's Swiftness; 
+/cast [stealth] Ravage
+/cast Ferocious Bite
+
 
 
 
