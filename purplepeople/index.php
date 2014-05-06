d50cf602aedda8bb896454a0e1c97d1aea80c014
ryan <ryan@proximify.ca>
2014-05-06 17:10:59 -0400

    added the initial index and outline for purple people eater

---
+++ purplepeople/index.php
@@ -0,0 +1,24 @@
+// images (image maps) of different parts
+// hats
+
+// moustaches & beards
+
+// sunglasses
+
+// eyes
+
+// teeth
+
+// cloths
+
+
+// the purple people eater
+
+
+
+// reset to starting positions
+
+// hold mousedown to drag and drop
+
+// whatever you click comes to the top
+
