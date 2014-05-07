454ec4048c81d9233b719a590460e649c82f02c0
Ryan <ryan@tobin.ca>
2014-05-07 00:07:20 -0400

    started a folder for a js plugin to show who is coming to an event

---
+++ Argylelabs/whoscoming/whoscoming.js
@@ -0,0 +1,30 @@
+<script>
+
+// who's coming
+
+// event page
+
+// title
+
+// who's coming
+
+// interactible box, with people listed who've already joined
+
+// + <-- to add yourself
+
+// Details
+
+// Click to +Add Me
+
+// Pic [From v] G, FB, Li [+ More]
+
+// Upload - Browse
+// drag and drop
+
+// crop
+
+// Types/targets/timelines/added who/closer contact/connection
+
+// Mobile First
+
+</script>
