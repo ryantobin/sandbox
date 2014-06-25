80aac38466b52d5bf16f0d19007b7c7dd2bf6942
Ryan <ryan@tobin.ca>
2014-06-25 13:22:58 -0400

    gamerproj

--- wow/index.php
+++ wow/index.php
@@ -1,1 +1,17 @@
 <!-- index.php-->
+
+Warrior Glyphs Chart
+
+http://www.elsprofessions.com/inscription/warrior_glyphs.html
+
+Profession Guide
+
+wow-professions.com
+
+
+streaming
+twitch
+
+Glyph Checklist Generator
+(for pvp) (for pve) grayed out otherwise, clickable checklist
+hot links to glyph thumbnail
