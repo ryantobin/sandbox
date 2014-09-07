f2c774493de82c55930361217363e182bc770660
ryan <ryan@proximify.ca>
2014-09-07 14:20:47 -0400

    updating to latest css old image of aging jeeves partial ingredient list

---
+++ wow/jeeves.php
@@ -0,0 +1,7 @@
+16x adamantite bar
+16x fel iron bolts = 16xfel iron bar
+2x khorium power core = 2xkhorium bar + 2xprimal fire
+
+8xtitansteel = 24xtitanium + 8x(eternal fire + earth + shadow)
+2xkings amber
+13xcobalt bolts
