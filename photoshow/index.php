b0f2f369c76f13da7a79ade41146a33cd3a8aa7e
Ryan <ryan@tobin.ca>
2014-09-10 18:40:31 -0400

    Some edits to photoshow, starting from scratch

--- photoshow/index.php
+++ photoshow/index.php
@@ -8,63 +8,63 @@
 <head>
   <title>Photoshow</title>
   <link type='text/css' rel='stylesheet' href='photoshow.css'>
-  <script src='../js/jquery-1.8.2.js'>
+  <script src='../js/jquery-1.8.2.js'/>
 </head>
 <script>
-
-/* get/create list of images stored in a folder */
-// folder the images are store in
-
-var images;
-
-$.ajax({
-  url: 'getFolderAsArrayOfNames.php',
-  dataType: 'json',
-  success: function(data) {
-    for(var i=0;i<data.length;i++) {
-      images.folder = data.folder;
-
-    }
-  });
-});
-
-var image;
-	// image.folder = "E:/Backups/WD%20Passport/Files/Photos%20&amp;%20Graphics";
-	image.folder = "E:/Backups/WD My Passport 500GB/Files/Photos & Graphics";
-
-
-// array of: img.name, img.height, img.width, img.size, img.location
-// add the first five images to the array
-
-  //?? get their dimensions 
-  //?? calculate heights on page for each ///TODO: (on page load and resize)
-
-/* add the images to the webpage */
-// create html entities with properties
-// img or background-image - each image, full screen to the window
-// id = their name
-
-// script to lazy load images (depending on how fast you scroll and how far down you scroll)
-
-// parallax to label days, places or events
-
-// get heights first
-
-// extra containing slider entity
-// slide similar pictures back and forth (only show the good ones but provide a way to show all
-
-// download - email - select/send/link
 </script>
 
 <?php
 
+// DEFINE FOLDER NAME HERE - your photo folder in the website directory
+$picturesFolderName = "pics";
+// $picturesPath = "../".$picturesFolderName;
+$picturesPath = $picturesFolderName;
+//echo $picturesPath;
+$listOfFilesInPicturesPath;
+$listOfImages = array();
+
+// GET FILE NAMES IN THE FOLDER
+//$listOfFilesInPicturesPath = scandir($picturesPath);
+$listOfFilesInPicturesPath = scandir("E:\Backups\Photos\Israel");
+
+//print_r($listOfFilesInPicturesPath);
+
+// If its an image add it to the listOfImages
+foreach ($listOfFilesInPicturesPath as $nameOfFile)
+{
+
+	//if (itIsAnImage)
+	if (endsWith('.jpg', $nameOfFile))
+	//if (true)
+	{
+		//echo "name:".$nameOfFile;
+		// append (nameOfFile) to (listOfImages)
+		//($listOfImages) += ($nameOfFile);	
+		array_push($listOfImages,$nameOfFile);
+	}
+}
+
+// create images with names of files
+foreach ($listOfImages as $nameOfFile)
+{
+	//echo '<img src="'+picturesPath+'/'+nameOfFile+'">';
+	//echo '<br>'.$picturesPath.'/'.$nameOfFile;
+	echo '<img src="'.$picturesPath.'/'.$nameOfFile.'" width="100%">';
+
+}
+/*
+*/
+// http://stackoverflow.com/questions/834303/startswith-and-endswith-functions-in-php
+/*function endsWith($needle, $haystack)
+{
+    return $needle === "" || substr($haystack, -strlen($needle)) === $needle;
+}*/
+function endsWith($needle, $haystack) {
+     return preg_match('/' . preg_quote($needle, '/') . '$/', $haystack);
+ }
+
 ?>
 <body>
-  <div id='content'>
-    <div id='cover'></div>
-    <div class='welcome-splash'></div>
-    <div class='first-splash'></div>
-    <div class='second-splash'></div>
-  </div>  
+<img src="pics/kitten_stella.jpg">
 </body>
 </html>
