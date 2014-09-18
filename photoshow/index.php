3963ce7148aa354ac6c70660122e6739f624abf2
Ryan <ryan@tobin.ca>
2014-09-18 10:09:34 -0400

    photoshow - displaying pics from a hard coded folder

--- photoshow/index.php
+++ photoshow/index.php
@@ -13,57 +13,34 @@
 <script>
 </script>
 
+<body>
+
 <?php
 
 // DEFINE FOLDER NAME HERE - your photo folder in the website directory
-$picturesFolderName = "pics";
 $picturesFolderName = "../../images/Pats";
-// $picturesPath = "../".$picturesFolderName;
-$picturesPath = $picturesFolderName;
-//echo $picturesPath;
-$listOfFilesInPicturesPath;
-$listOfImages = array();
+$picturesFolderName = "pics";
+$picturesFolderName = "";
 
-// GET FILE NAMES IN THE FOLDER
-$listOfFilesInPicturesPath = scandir($picturesPath);
-
-// If its an image add it to the listOfImages
-foreach ($listOfFilesInPicturesPath as $nameOfFile)
-//foreach ($testFolder as $nameOfFile)
-{
-
-	//if (itIsAnImage)
-	if (endsWith('.jpg', $nameOfFile) || endsWith('.JPG', $nameOfFile) || endsWith('.png', $nameOfFile) || endsWith('.PNG', $nameOfFile))
-	// if (true)
-	{
-		// echo "name:".$nameOfFile;
-		// append (nameOfFile) to (listOfImages)
-		//($listOfImages) += ($nameOfFile);	
-		array_push($listOfImages,$nameOfFile);
-	}
-}
-
-// create images with names of files
-foreach ($listOfImages as $nameOfFile)
-{
-	//echo '<img src="'+picturesPath+'/'+nameOfFile+'">';
-	//echo '<br>'.$picturesPath.'/'.$nameOfFile;
-	echo '<img src="'.$picturesPath.'/'.$nameOfFile.'" class="photoimage" width="100%">';
-
-}
-/*
-*/
-// http://stackoverflow.com/questions/834303/startswith-and-endswith-functions-in-php
-/*function endsWith($needle, $haystack)
-{
-    return $needle === "" || substr($haystack, -strlen($needle)) === $needle;
-}*/
-function endsWith($needle, $haystack) {
-     return preg_match('/' . preg_quote($needle, '/') . '$/', $haystack);
- }
+// displays images in individual image tags
+include 'display_pics.php';
 
 ?>
-<body>
-<img src="pics/kitten_stella.jpg">
+
+	<div id='stella_container'>
+		<img src="pics/kitten_stella.jpg" onclick='moveRight(this)'>
+	</div>
 </body>
+
+<script>
+
+// click image to slide right
+function moveRight(elem) {
+	// alert('click, triggered moveRight'+elem.toString());
+	elem.setAttribute('src','pics/kitten_stella_IMG_6455.jpg');
+	elem.setAttribute('width','100%');
+}
+
+</script>
+
 </html>
