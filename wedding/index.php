2a2b292031c0a9a7b519d0b894f6d98b91741947
Ryan <ryan@tobin.ca>
2014-11-17 00:51:27 -0500

    wedding and wow changes

--- wedding/index.php
+++ wedding/index.php
@@ -1,34 +1,29 @@
+<?php include ('head.php'); ?>
 
-<!DOCTYPE html>
-<html>
-
-<head>
-	<link href="wedding.css" rel="stylesheet" type="text/css">
-	<link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
-</head>
-
-<body>
-	<div id='content'>
-		<div id='top-banner'>
-			<span> August 2, 2015</span>
-			<span class='names'>Kristen &amp; Ryan</span>
-			<span> Wakefield, QC</span>
-		</div>
-		<nav id='main-menu'>
-			<a href='#' class='current-page'><span>Home</span></a>
-			<a href='our-story.php'><span>Our Story</span></a>
-			<a href='events-info.php'><span>Events &amp; Day-Of Info</span></a>
-			<a href='accommodation.php'><span>Accommodation</span></a>
-			<a href='rsvp.php'><span>RSVP</span></a>
-		</nav>
-		</div>
-		<div id='welcome-slider'></div>
-		<div id='bottom-banner'></div>
-
-		<div id='note'>For all you out-of-towners, check out events and accommodation. <br>Please RSVP by Christmas, 2015.</div>
-
-		<div id='bottom-menu'></div>
+<div id='content'>
+	<div id='top-banner'>
+		<span> August 2, 2015</span>
+		<span class='names'>Kristen &amp; Ryan</span>
+		<span> Wakefield, QC</span>
 	</div>
-	<div id='footer'></div>
-</body>
-</html>
+	<nav id='main-menu'>
+		<a href='#' class='current-page'><span>Home</span></a>
+		<a href='our-story.php'><span>Our Story</span></a>
+		<a href='events-info.php'><span>Events &amp; Day-Of Info</span></a>
+		<a href='accommodation.php'><span>Accommodation</span></a>
+		<a href='rsvp.php'><span>RSVP</span></a>
+	</nav>
+	</div>
+	<div id='welcome-slider'></div>
+	<div id='bottom-banner'></div>
+
+	<div id='note'>For all you out-of-towners, check out <a href='events.php'>events</a> and <a href='accommodation.php'>accommodation</a>.
+		<br>We'll have more information available for people as we get closer to the date,
+		<br>and let you know the night's menu options.
+		<p>The date is set!</p>
+		<p>Please RSVP by Christmas, 2015.</p>
+
+	<div id='bottom-menu'></div>
+</div>
+
+<?php include ('footer.php');
---
+++ wow/macros/index.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title>
+
+</title>
+<link rel='stylesheet' type='text/css' href='../wow.css'>
+</head>
+
+<body>
+	
+<?php
+
+include('structure.php');
+// click class
+
+// (enter) file name
+// view macros
+// pre
+// filter / search
+// copy
+
+?>
+</body>
+</html>
