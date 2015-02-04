b7e2274d927a66994dcf7f81a4bfc0a044684c71
Ryan <ryan@tobin.ca>
2015-02-04 15:22:25 -0500

    Removed footer.php Renamed accomodation --> accomations.php Added wedding.less

---
+++ wedding/accommodations.php
@@ -0,0 +1,22 @@
+
+<?php include ('head.php'); ?>
+
+<body>
+	<div id='content'>
+		<?php include 'top.php'; ?>
+		<div id='accommodations-body-bg'></div>
+		<div id='accommodations-body'>
+			<header>Accommodations</header>
+			<p>
+				Please check out this page in the coming months.  We hope to provide you with group rates and recommendations in Ottawa &amp; the Wakefield area.
+			</p>
+			<p>
+				Some of the groom's family will be staying at the <a href='http://www.hilton.com/search/hi/ca/qc/gatineau-ottawa/0/0000000000/0/0/0/0/30?WT.srch=1' target="_blank">Hilton Lac-Leamy</a>.
+			</p>
+			</div>
+		</div>
+	</div>
+
+</body>
+
+</html>
--- wedding/footer.php
+++
@@ -1,7 +0,0 @@
-<div id='footer'>
-<!--...
--->
-</div>
-
-</body>
-</html>
